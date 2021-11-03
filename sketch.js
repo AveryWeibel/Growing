aa198e9619b9b5f58665bbe0c08b5c1bd9b3e688
mrzebrasmoothie <averyweibel@yahoo.com>
2021-11-02 17:08:17 -0700

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400, WEBGL);
+}
+
+function draw() {
+  background(220);
+  cone()
+  rotateX(45)
+  cone()
+    rotateX(45)
+  cone()
+    rotateX(45)
+  cone()
+    rotateX(45)
+  cone()
+}
+ 
